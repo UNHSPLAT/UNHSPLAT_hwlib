849d42c6ce814be5c00dc0bf7cc4325d57c3eb32
Colin van Ysseldyk <colin.vanysseldyk@unh.edu>
2022-02-15 11:51:18 -0700

    Add support for Leybold Center 2

---
+++ PressureSensors/leyboldCenter2.m
@@ -0,0 +1,53 @@
+classdef leyboldCenter2 < hwDevice
+    %LEYBOLDCENTER2 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties (Constant)
+        Type string = "Pressure Sensor"
+        ModelNum string = "Center 2"
+    end
+    
+    methods
+        function obj = leyboldCenter2(address)
+            %LEYBOLDCENTER2 Construct an instance of this class
+            %   Detailed explanation goes here
+            obj@hwDevice(address);
+
+            obj.hVisa.BaudRate = 9600;
+            obj.hVisa.Terminator = 'CR';
+        end
+        
+        function dataOut = leyboldRW(obj,inStr)
+
+            retMsg = obj.devRW(inStr);
+
+            if retMsg(1) == 6
+                dataOut = obj.devRW(char(5));
+            else
+                error("leyboldCenter2:notAcknowledged","Communication error! Message not acknowledged by controller...");
+            end
+
+        end
+
+        function pressure = readPressure(obj,sensorNum)
+
+            if ~exist('sensorNum','var')
+                sensorNum = 1:2;
+            end
+
+            pressure = zeros(1,length(sensorNum));
+            for iS = 1:length(sensorNum)
+                dataOut = obj.leyboldRW(['PR',num2str(sensorNum(iS))]);
+                tokes = regexp(strtrim(dataOut),char(32),'split');
+                if str2double(tokes{1}) > 2
+                    pressure(iS) = NaN;
+                    warning("leyboldCenter2:sensorUnconnected","No pressure sensor connected on output %i...",sensorNum(iS));
+                else
+                    pressure(iS) = str2double(tokes{2});
+                end
+            end
+
+        end
+    end
+end
+
