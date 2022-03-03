1015b8569681a98f2c38b5bcd3d99f079065f45b
Colin van Ysseldyk <colin.vanysseldyk@unh.edu>
2022-03-03 17:42:58 -0500

    Add abstract class for SRS HVPSs

---
+++ Abstract/srsHVPS.m
@@ -0,0 +1,94 @@
+classdef srsHVPS < powerSupply
+    %SRSHVPS Summary of this class goes here
+    %   Detailed explanation goes here
+
+    properties (Constant)
+        NumOutputs double = 1
+    end
+
+    methods
+        function obj = srsHVPS(address)
+            %SRSHVPS Construct an instance of this class
+            %   Detailed explanation goes here
+            obj@powerSupply(address);
+
+            obj.getAllSettings;
+        end
+
+        function setVSet(obj,volt)
+
+            obj.devRW(['VSET',num2str()]);
+            obj.VSet = volt;
+
+        end
+
+        function volt = getVSet(obj)
+
+            dataOut = obj.devRW('VSET?');
+            volt = str2double(strtrim(dataOut));
+
+            obj.VSet = volt;
+
+        end
+
+        function volt = measV(obj)
+
+            dataOut = obj.devRW('VOUT?');
+            volt = str2double(strtrim(dataOut));
+            
+        end
+
+        function setISet(obj,curr)
+
+            obj.devRW(['ILIM',num2str()]);
+            obj.ISet = curr;
+
+        end
+
+        function curr = getISet(obj)
+
+            dataOut = obj.devRW('ILIM?');
+            curr = str2double(strtrim(dataOut));
+
+            obj.ISet = curr;
+
+        end
+
+        function curr = measI(obj)
+
+            dataOut = obj.devRW('IOUT?');
+            curr = str2double(strtrim(dataOut));
+
+        end
+
+        function setOutputState(obj,state)
+
+            if state
+                obj.devRW('HVON');
+                obj.OutputState = true;
+            else
+                obj.devRW('HVOF');
+                obj.OutputState = false;
+            end
+
+        end
+
+        function state = getOutputState(obj)
+
+            obj.devRW('HVOF');
+            obj.OutputState = false;
+            state = false;
+
+        end
+
+        function [volt,curr,state] = getAllSettings(obj)
+
+            volt = obj.getVSet;
+            curr = obj.getISet;
+            state = obj.getOutputState;
+
+        end
+
+    end
+end
+
--- PowerSupplies/srsPS350.m
+++ PowerSupplies/srsPS350.m
@@ -1,10 +1,9 @@
-classdef srsPS350 < powerSupply
+classdef srsPS350 < srsHVPS
     %SRSPS350 Summary of this class goes here
     %   Detailed explanation goes here
 
     properties (Constant)
         ModelNum string = "PS350"
-        NumOutputs double = 1
         VMin double = 0
         VMax double = 5000
         IMin double = 0
@@ -21,85 +20,9 @@ classdef srsPS350 < powerSupply
         function obj = srsPS350(address)
             %SRSPS350 Construct an instance of this class
             %   Detailed explanation goes here
-            obj@powerSupply(address);
-
-            obj.getAllSettings;
-        end
-
-        function setVSet(obj,volt)
-
-            obj.devRW(['VSET',num2str()]);
-            obj.VSet = volt;
+            obj@srsHVPS(address);
 
         end
-
-        function volt = getVSet(obj)
-
-            dataOut = obj.devRW('VSET?');
-            volt = str2double(strtrim(dataOut));
-
-            obj.VSet = volt;
-
-        end
-
-        function volt = measV(obj)
-
-            dataOut = obj.devRW('VOUT?');
-            volt = str2double(strtrim(dataOut));
-            
-        end
-
-        function setISet(obj,curr)
-
-            obj.devRW(['ILIM',num2str()]);
-            obj.ISet = curr;
-
-        end
-
-        function curr = getISet(obj)
-
-            dataOut = obj.devRW('ILIM?');
-            curr = str2double(strtrim(dataOut));
-
-            obj.ISet = curr;
-
-        end
-
-        function curr = measI(obj)
-
-            dataOut = obj.devRW('IOUT?');
-            curr = str2double(strtrim(dataOut));
-
-        end
-
-        function setOutputState(obj,state)
-
-            if state
-                obj.devRW('HVON');
-                obj.OutputState = true;
-            else
-                obj.devRW('HVOF');
-                obj.OutputState = false;
-            end
-
-        end
-
-        function state = getOutputState(obj)
-
-            obj.devRW('HVOF');
-            obj.OutputState = false;
-            state = false;
-
-        end
-
-        function [volt,curr,state] = getAllSettings(obj)
-
-            volt = obj.getVSet;
-            curr = obj.getISet;
-            state = obj.getOutputState;
-
-        end
-
     end
 end
 
