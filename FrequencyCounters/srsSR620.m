d4479f898aafbaafc6aff6582dd295039ab27b52
UNHSplat <unhsplat@gmail.com>
2026-01-06 18:28:46 -0500

    added async to counter

--- FrequencyCounters/srsSR620.m
+++ FrequencyCounters/srsSR620.m
@@ -26,6 +26,24 @@ classdef srsSR620 < frequencyCounter
             display(dataOut);
 
         end
+
+        function measure_async(obj)
+            function read_async(~,~)
+                dataOut = fscanf(obj.hVisa);
+                display(dataOut);
+                obj.dataOut = dataOut;
+                obj.lastRead = str2double(strtrim(dataOut));
+                obj.lastReadTime = datetime('now');
+
+                obj.hVisa.BytesAvailableFcn = @(~,~) nan;
+                if strcmp(obj.hVisa.Status,'open')
+                    flushoutput(obj.hVisa);
+                    flushinput(obj.hVisa);
+                    fclose(obj.hVisa);
+                end
+            end
+            obj.devRW_async('MEAS? 0',@read_async);
+        end
     end
 end
 
