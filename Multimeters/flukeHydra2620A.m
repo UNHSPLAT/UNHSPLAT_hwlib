bfccbf25d4f4d2645e0b2422de11b13acf7ac3c9
jonbowr <jonbowr@yahoo.com>
2026-03-06 11:42:58 -0500

    added async read for fluke

--- Multimeters/flukeHydra2620A.m
+++ Multimeters/flukeHydra2620A.m
@@ -19,31 +19,25 @@ classdef flukeHydra2620A < multimeter
             obj.lastRead = [nan,nan,nan,nan];
 
             function stuff = read_it(self)
-                stuff = self.scan();
+                stuff = self.read_scan();
             end
             obj.readFunc =@read_it;
         end
-        
-        function val = measure(obj)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
 
-            dataOut = obj.devRW("measure?");
-            val = str2double(strtrim(dataOut));
-        end
-
-        function dataOut = initThenRead(obj)
-
-            obj.devRW('INIT');
-            obj.devRW('*WAI');
-            dataOut = obj.devRW(':READ?');
-
-        end
-
-        function dataOut = scan(obj)
+        function dataOut = read_scan(obj)
             dataOut = obj.devRW('LAST?');
             tokes = regexp(strtrim(dataOut),',','split');
             dataOut = str2double(tokes);
+        end
+
+        function ask_async(obj)
+            obj.devRW_async('LAST?',@obj.read_async);
+        end
+
+        function read_async(obj)
+            obj.devR_async();
+            tokes = regexp(strtrim(obj.dataOut),',','split');
+            obj.lastRead = str2double(tokes);
         end
     end
 end
